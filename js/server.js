a8d7a4d748a1be73ea32519c973f486cd0c4c976
Sommos <Sommosgaming@gmail.com>
2023-08-01 16:47:14 +0100

    added async function to reduce nested functions

--- js/server.js
+++ js/server.js
@@ -34,18 +34,38 @@ console.log(path.extname(__filename));
 console.log(path.parse(__filename));
 */
 
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
+const fileOps = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'assets', 'lorem.txt'), 'utf8');
+        console.log(data);
+        await fsPromises.writeFile(path.join(__dirname, 'assets', 'promise_write.txt'), data);
+        await fsPromises.appendFile(path.join(__dirname, 'assets', 'promise_write.txt'), '\n\nPromise Append File Test.');
+        await fsPromises.rename(path.join(__dirname, 'assets', 'promise_write.txt'), path.join(__dirname, 'assets', 'new_promise_write.txt'));
+        const newData = await fsPromises.readFile(path.join(__dirname, 'assets', 'new_promise_write.txt'), 'utf8');
+        console.log(newData);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+fileOps();
+
+/*
 fs.readFile(path.join(__dirname, 'assets', 'lorem.txt'), 'utf8', (err, data) => {
     if (err) {
         throw err;
     }
     console.log(data.toString());
 });
+*/
 
+/*
 // asynchronous allows for this to run while the file is being read
 console.log('Hello...');
+
 
 fs.writeFile(path.join(__dirname, 'assets', 'write.txt'), 'Write File Test.', (err) => {
     if (err) {
@@ -58,6 +78,13 @@ fs.writeFile(path.join(__dirname, 'assets', 'write.txt'), 'Write File Test.', (e
             throw err;
         }
         console.log('Append Complete');
+
+        fs.rename(path.join(__dirname, 'assets', 'write.txt'), path.join(__dirname, 'assets', 'new_write.txt'), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('Rename Complete');
+        });
     });
 });
 
@@ -65,3 +92,5 @@ process.on('uncaughtException', err => {
     console.error(`There was an uncaught error: , ${err}`);
     process.exit(1);
 });
+
+*/
