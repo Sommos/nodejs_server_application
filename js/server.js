7c81e7dbba7cafe6ae5d1885fa06ea59636fbae7
Sommos <Sommosgaming@gmail.com>
2023-08-01 01:10:08 +0100

    added math.js, destructured module imports

--- js/server.js
+++ js/server.js
@@ -1,11 +1,23 @@
 // nodejs runs on a server, not in a clients browser
-console.log("Hello World!");
+// console.log("Hello World!");
 // global object instead of a window object
-console.log(global);
+// console.log(global);
 // import modules
 const os = require('os');
 const path = require('path');
+const {
+    add, 
+    subtract, 
+    multiply, 
+    divide,
+} = require('./math');
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
 // use the imported modules to get info about the server
+/*
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
@@ -16,6 +28,7 @@ console.log(__filename);
 console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename));
+*/
 
 // print out an object with all the info about the path
-console.log(path.parse(__filename));
+// console.log(path.parse(__filename));
